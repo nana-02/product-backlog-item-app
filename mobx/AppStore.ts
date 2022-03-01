a2cb68accb8335b3e7818b9c9a86282db95747f6
nana-02 <s.nkmr.95.bb@gmail.com>
2022-03-01 16:43:01 +0900

    永続化　完了

--- mobx/AppStore.ts
+++ mobx/AppStore.ts
@@ -1,18 +1,24 @@
 import { action, observable, makeObservable} from 'mobx';
-import defaultForm from '../data/defaultForm.json'
-import { IItem } from '../interface/IDraggableList'
-
+import defaultForm from '../data/defaultForm.json';
+import { IItem } from '../interface/IDraggableList';
+import { persist, create } from 'mobx-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 class AppStore {
-  @observable.ref form: IItem[] = defaultForm;
+  @persist('list') @observable form: IItem[] = defaultForm;
 
   @action setFormData(newForm: IItem[]){
     this.form = newForm;
   }
-
   constructor() {
     makeObservable(this);
   }
 }
+const hydrate = create({
+  storage: AsyncStorage,  
+  jsonify: true
+})
 
-const appStore = new AppStore();
+// create the state
+const appStore = new AppStore()
+hydrate('appStore', appStore).then(() => console.log('appStore has been hydrated'))
 export default appStore;
